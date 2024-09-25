2d968b0844a9d4829a1cc986502ce310c1b2ed64
Hossein Kimiyavi <hosseinkimiyavi@gmail.com>
2024-09-25 23:31:50 +0330

    creat a card component and styles

---
+++ src/Components/Card.jsx
@@ -0,0 +1,29 @@
+
+import { Link } from "react-router-dom";
+import { TbListDetails } from "react-icons/tb";
+import { TbShoppingBagCheck } from "react-icons/tb";
+import { shortenText } from "../Helpers/helper";
+import styles from "./card.module.css";
+function Card({data} ) {
+  const { title, id, price, image } = data;
+  return (
+    <div className={styles.card}>
+      <img src={image} alt={title} />
+      <h3>{shortenText(title)}</h3>
+      <p>{price} $</p>
+      <div className={styles.actions}>
+        <Link to={`/product/${id}`}>
+          <TbListDetails />
+        </Link>
+        <div>
+          <button>
+            <TbShoppingBagCheck />
+          </button>
+        </div>
+      </div>
+     
+    </div>
+  );
+}
+
+export default Card;
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,16 +1,17 @@
+import Card from "../Components/Card";
 import { useProduct } from "../Context/ProductProvider";
 import styles from "../pages/productPage.module.css"
 function ProductPage() {
   const products = useProduct();
-  console.log(products);
+  
   return (
     <>
       <div className={styles.container}>
         <div className={styles.products}>
+          {!products.length && (<p>loading...</p>)}
           {products.map((p) => (
-            <p key={p.id}>{p.title}</p>
+            <Card key={p.id} data={p} />
           ))}
-          {!products.lenght && (<p>loading...</p>)}
         </div>
         <div>Side Bar</div>
       </div>
