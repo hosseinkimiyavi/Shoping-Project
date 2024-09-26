e8d1e4200c237d4a2d57abb2b9b44301a4343524
Hossein Kimiyavi <hosseinkimiyavi@gmail.com>
2024-09-26 21:28:27 +0330

    creat a category filter for products

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,4 +1,5 @@
 import Card from "../Components/Card";
+import { FaListUl } from "react-icons/fa";
 import { ImSearch } from "react-icons/im";
 import Loader from "../Components/Loader";
 import { useProduct } from "../Context/ProductProvider";
@@ -6,9 +7,16 @@ import styles from "../pages/productPage.module.css"
 import { useState } from "react";
 function ProductPage() {
   const products = useProduct();
+ 
   const [search,setsearch] =useState("");
   const SearchHandler =()=>{
 
+  }
+  const categoryHandler =(event)=>{
+    const { tagName } = event.target;
+    const category = event.target.innerText.toLocaleLowerCase()
+    if(tagName!="LI") return;
+    console.log(category);
   }
   return (
     <>
@@ -23,7 +31,19 @@ function ProductPage() {
             <Card key={p.id} data={p} />
           ))}
         </div>
-        <div>Side Bar</div>
+        <div>
+          <div>
+            <FaListUl />
+            <p>Categories</p>
+          </div>
+          <ul onClick={categoryHandler}>
+            <li>All</li>
+            <li>Electronics</li>
+            <li>Jewelery</li>
+            <li>Men's Clothing</li>
+            <li>Women's Clothing</li>
+          </ul>
+        </div>
       </div>
     </>
   );
