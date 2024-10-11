0eeae0074fc82c8d777b42abf50cb8c596c8d3f1
Hossein Kimiyavi <hosseinkimiyavi@gmail.com>
2024-10-11 14:36:16 +0330

    add Basket card component

---
+++ src/Components/BasketCard.jsx
@@ -0,0 +1,21 @@
+
+import { shortenText } from '../Helpers/helper'
+import { MdDeleteOutline } from 'react-icons/md'
+import styles from "./Basket.module.css"
+function BasketCard({data,clickHandler}) {
+  return (
+    <div className={styles.card}>
+        <img src={data.image} alt={data.title} />
+        <p>{shortenText(data.title)}</p>
+        <div className={styles.actions}>
+            {data.quantity ==1 && (<button onClick={()=>clickHandler("REMOVE_ITEM",data)}><MdDeleteOutline /></button>)}
+            {data.quantity >1 && <button onClick={()=>clickHandler("DECREASE",data)}>-</button>}
+            <span>{data.quantity}</span>
+            <button onClick={()=>clickHandler("INCREASE",data)}>+</button>
+
+        </div>
+    </div>
+  )
+}
+
+export default BasketCard
--- src/pages/CheckOutpage.jsx
+++ src/pages/CheckOutpage.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-
+import BasketCard from "../Components/BasketCard";
+import { useCard } from "../Context/CartContext";
 function CheckOutpage() {
+  const [state,dispatch] =useCard();
+  const clickHandler =(type,payload)=>dispatch({type,payload})
   return (
-    <div>CheckOutpage</div>
+    <div>
+      <div>
+        {state.selectedItem.map((product)=>(
+          <BasketCard key={product.id} data={product} clickHandler={clickHandler} />
+        )
+
+        )}
+      </div>
+    </div>
   )
 }
 
